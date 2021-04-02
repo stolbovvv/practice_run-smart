55ef11b6f1a24d6f2e1ccac891181b58cf45933f
Valerii Stolbov <61787196+stolbovvv@users.noreply.github.com>
2021-04-02 19:18:18 +0500

    create section CATALOG 2021-04-02

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,6 @@ $(document).ready(function () {
         slidesToShow: 1,
 
         responsive: [{
-
             breakpoint: 769,
             settings: {
                 arrows: false,
@@ -19,9 +18,27 @@ $(document).ready(function () {
     $('.catalog__triggers-item').click(function () {
         $('.catalog__triggers-item').removeClass('catalog__triggers-item_active');
         $('.catalog__content-item').removeClass('catalog__content-item_active');
+        $('.catalog__card-more').removeClass('catalog__card-more_active');
 
         $(this).addClass('catalog__triggers-item_active');
         $(`#${$(this).attr('data-tab-ibdex')}`).addClass('catalog__content-item_active');
     });
+
+    $('.catalog__card-link-more').click(function (e) {
+        e.preventDefault();
+        $('.catalog__card-more').removeClass('catalog__card-more_active');
+        $(this)
+            .parents('.catalog__card-wrap-info')
+            .children('.catalog__card-more')
+            .addClass('catalog__card-more_active');
+    })
+
+    $('.catalog__card-link-back').click(function (e) {
+        e.preventDefault();
+        $(this)
+            .parents('.catalog__card-more')
+            .removeClass('catalog__card-more_active');
+    })
+
 
 });
