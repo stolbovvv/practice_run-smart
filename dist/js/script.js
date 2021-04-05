b345a35cc741b5d2054ccfc4672267536590c05e
Valerii Stolbov <61787196+stolbovvv@users.noreply.github.com>
2021-04-05 17:26:41 +0500

    add validate and mask for forms 2021-04-05

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // SLICK SLIDER
     $('.watch-slider__content').slick({
         prevArrow: '<img class="watch-slider__arrow watch-slider__arrow_prev" src="img/icons/icon-arrow-slider.svg" alt="prev"></img>',
         nextArrow: '<img class="watch-slider__arrow watch-slider__arrow_next" src="img/icons/icon-arrow-slider.svg" alt="prev"></img>',
@@ -15,6 +16,7 @@ $(document).ready(function () {
         }]
     });
 
+    // CATALOG TABS AND MORE
     $('.catalog__triggers-mobile').change(function () {
         $('.catalog__content-item').removeClass('catalog__content-item_active');
         $('.catalog__card-more').removeClass('catalog__card-more_active');
@@ -47,6 +49,7 @@ $(document).ready(function () {
             .removeClass('catalog__card-more_active');
     })
 
+
     // MODAL:
     $('.modal__close').click(function () {
         $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
@@ -63,8 +66,44 @@ $(document).ready(function () {
         })
     })
 
-    // $('.catalog__card-button').each(function (i) {
 
-    // });
+    // VALIDATE FORMS
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2,
+                },
+                phone: {
+                    required: true,
+                    minlength: 18,
+                },
+                email: {
+                    required: true,
+                    email: true,
+                },
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста введите свое имя!",
+                    minlength: jQuery.validator.format("Требуется как минимум {0} символов!"),
+                },
+                phone: {
+                    required: "Пожалуйста введите номер телефона!",
+                    minlength: jQuery.validator.format("Требуется как минимум 11 символов!"),
+                },
+                email: {
+                    required: "Пожалуйста укажите свою почту!",
+                    email: "Не верный формат. Пример: exemple@email.com",
+                },
+            }
+        })
+    }
 
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask('+7 (000) 000-00-00');
 });
